a0d6eadd9d9c6f196924d0fec72b15e3f9a9401d
Valeria Ferreira Nocua <78361578+valeferreiraan@users.noreply.github.com>
2023-11-01 12:52:07 -0500

    clase

---
+++ assets/js/introduccion.js
@@ -0,0 +1,56 @@
+//introducción a js
+//scripts ECMaScritps isomórfico
+//trabaja bajo secuencias
+
+//variables
+/*$name
+_name
+name
+//no debe contener espacios
+
+let numero = 1;
+
+let nombre = 'Taylor';
+
+let miVariable = true;
+
+let miVariableArr = [1, 'bob'];
+
+let suma = a+b
+
+pi = 3.1416
+const pi 
+
+*/
+
+
+//console.log(`el número ${a} es muy bonito`); //tiene que ser con comillas invertidas.
+
+
+//ejercicio 1
+
+let a  = 5;
+let b = 10;
+
+console.log(`la suma es ${a+b}`)
+
+
+/*operadores
+    aritmeticos + - * / mod %
+    comparación == igual o  identico === != !==
+    < > <= >=
+    logicos && || !
+*/
+
+
+let num1 = parseInt(prompt('digite el primer numero'));
+let num2 = parseInt(prompt('digite el segundo número'))
+
+
+
+console.log(`la suma es ${num1+num2}`)
+
+
+//funcion que haga la suma de dos numeros
+// una especie de calculadora
+
