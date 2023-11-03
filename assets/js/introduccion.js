984ddfb853be3ec8d1f1c07c984fad98ec959df6
Valeria Ferreira Nocua <78361578+valeferreiraan@users.noreply.github.com>
2023-11-02 21:03:30 -0500

    calculadora en js

--- assets/js/introduccion.js
+++ assets/js/introduccion.js
@@ -2,7 +2,7 @@
 //scripts ECMaScritps isomórfico
 //trabaja bajo secuencias
 
-//variables
+//letiables
 /*$name
 _name
 name
@@ -42,7 +42,7 @@ console.log(`la suma es ${a+b}`)
     logicos && || !
 */
 
-
+/*
 let num1 = parseInt(prompt('digite el primer numero'));
 let num2 = parseInt(prompt('digite el segundo número'))
 
@@ -50,7 +50,52 @@ let num2 = parseInt(prompt('digite el segundo número'))
 
 console.log(`la suma es ${num1+num2}`)
 
-
+*/
 //funcion que haga la suma de dos numeros
 // una especie de calculadora
 
+function suma(a, b) {
+    if (!isNaN(a) && !isNaN(b)) {
+      let resultado = a + b;
+      console.log("La suma de " + a + " y " + b + " es: " + resultado);
+    } else {
+      console.log("Por favor, ingrese números válidos.");
+    }
+  }
+  
+function resta(a, b) {
+    if (!isNaN(a) && !isNaN(b)) {
+      let resultado = a - b;
+      console.log("La resta de " + a + " y " + b + " es: " + resultado);
+    } else {
+      console.log("Por favor, ingrese números válidos.");
+    }
+  }
+
+function multip(a, b) {
+    if (!isNaN(a) && !isNaN(b)) {
+      let resultado = a * b;
+      console.log("La multiplicación de " + a + " y " + b + " es: " + resultado);
+    } else {
+      console.log("Por favor, ingrese números válidos.");
+    }
+  }
+
+function divi(a, b) {
+    if (!isNaN(a) && !isNaN(b) && b!== 0) {
+      let resultado = a / b;
+      console.log("La división de " + a + " y " + b + " es: " + resultado);
+    } else {
+      console.log("Por favor, ingrese números válidos.");
+    }
+  }
+
+
+
+let num1 = parseFloat(prompt("Ingrese el primer número:"));
+let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+  
+suma(num1, num2);
+resta(num1, num2);
+multip(num1, num2);
+divi(num1, num2);
